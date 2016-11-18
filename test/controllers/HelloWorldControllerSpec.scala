f4b3f0f00f7b56ed648472d32bd6a72cf9a2e154
matthew.page <matthew.page@digital.hmrc.gov.uk>
2016-11-18 09:34:35 +0000

    SAR-47: Removed namespacing from project

--- test/controllers/HelloWorldControllerSpec.scala
+++ test/controllers/HelloWorldControllerSpec.scala
@@ -14,28 +14,21 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.incometaxsubscription.controllers
+package controllers
 
 import play.api.http.Status
 import play.api.test.FakeRequest
-import play.api.http.Status
-import play.api.test.FakeRequest
-import uk.gov.hmrc.play.test.UnitSpec
-import uk.gov.hmrc.play.test.WithFakeApplication
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
 
-class MicroserviceHelloWorldControllerSpec extends UnitSpec with WithFakeApplication{
+class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication{
 
   val fakeRequest = FakeRequest("GET", "/")
 
-
   "GET /" should {
     "return 200" in {
-      val result = MicroserviceHelloWorld.hello()(fakeRequest)
+      val result = HelloWorldController.hello()(fakeRequest)
       status(result) shouldBe Status.OK
     }
   }
-
-
 }
