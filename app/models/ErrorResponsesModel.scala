c62199cb44a881832051380d39301ec2d19046b5
matthew.page <matthew.page@digital.hmrc.gov.uk>
2016-11-28 09:56:08 +0000

    add-missing-headers: Updated MicroSerice.scala and plugins.sbt to support scalastyle, scoverage and auto added missing headers

--- app/models/ErrorResponsesModel.scala
+++ app/models/ErrorResponsesModel.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models
 
 import play.api.libs.json.{JsValue, Json, Writes}
@@ -20,6 +36,3 @@ case object ErrorGenericBadRequest extends ErrorResponseModel(400, "BAD_REQUEST"
 case object ErrorAcceptHeaderInvalid extends ErrorResponseModel(406, "ACCEPT_HEADER_INVALID", "The accept header is missing or invalid")
 
 case object ErrorInternalServerError extends ErrorResponseModel(500, "INTERNAL_SERVER_ERROR", "Internal server error")
-
-
-
